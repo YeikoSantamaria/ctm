b0628d69e3e1155fb50ffb5a922cc2f1b85dc89a
Yeiko Santamaria <57649843+YeikoSantamaria@users.noreply.github.com>
2022-06-05 10:35:07 -0500

    of

--- docs-ctm/static/scripts.js
+++ docs-ctm/static/scripts.js
@@ -4,3 +4,15 @@
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "5y9i3ki4gl");
+
+
+
+    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-LKTYR7JN2M"></script>
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-LKTYR7JN2M');
