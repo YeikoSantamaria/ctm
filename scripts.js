95b68e0942a7e29f56c702b670a6fcabd8aa57aa
Yeiko Santamaria <57649843+YeikoSantamaria@users.noreply.github.com>
2022-06-05 10:42:36 -0500

    analitycs

--- scripts.js
+++ scripts.js
@@ -4,3 +4,13 @@
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "9y5d1kjvsq");
+
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-LKTYR7JN2M"></script>
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-LKTYR7JN2M');
